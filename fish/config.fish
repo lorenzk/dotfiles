4fcea6653c5b478f847c700f30b74f1f12eaee91
Lorenz Kitzmann <lorenz@lorenzkitzmann.com>
2016-02-22 10:39:40 +0100

    use test -f syntax instead of []

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 status --is-interactive; and . (rbenv init -|psub)
 
 # autojump
-[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+test -f /usr/local/share/autojump/autojump.fish; and . /usr/local/share/autojump/autojump.fish
 
 alias vi nvim
 alias l "ls -lF"
